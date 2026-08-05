962251d6e5a3570b6f172b64a3937a1abf9da980
Pragatheeswara5 <ptw.karl@gmail.com>
2026-08-05 08:05:35 +0530

    ngrx Selectors and async pipe

--- src/app/counter/counter-value/counter-value.ts
+++ src/app/counter/counter-value/counter-value.ts
@@ -1,26 +1,22 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { CounterState } from '../states/counter.state';
+import { getCounter } from '../states/counter.selector';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-counter-value',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './counter-value.html',
   styleUrl: './counter-value.css',
 })
-export class CounterValue implements OnInit, OnDestroy{
+export class CounterValue implements OnInit{
   constructor(private store: Store<{ counter: CounterState }>) {}
-  counter: number = 0;
-  counterSubscription: Subscription | null = null;
+  counter$: Observable<number> | null = null;
 
   ngOnInit(): void {
-    this.counterSubscription = this.store.select('counter').subscribe((value) => {
-      this.counter = value.counter;
-    });
+    this.counter$=this.store.select(getCounter);
   }
 
-  ngOnDestroy(): void {
-      if(this.counterSubscription) this.counterSubscription.unsubscribe();
-  }
 }
--- src/app/counter/custom-input/custom-input.ts
+++ src/app/counter/custom-input/custom-input.ts
@@ -1,13 +1,15 @@
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CounterState } from '../states/counter.state';
 import { Store } from '@ngrx/store';
 import { customIncrement, customToggleBtn } from '../states/counter.actions';
+import { getToggle } from '../states/counter.selector';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-custom-input',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, AsyncPipe],
   templateUrl: './custom-input.html',
   styleUrl: './custom-input.css',
 })
@@ -16,12 +18,10 @@ export class CustomInput implements OnInit{
   constructor(private store:Store<{counter:CounterState}>){}
 
   customValue:number=0;
-  togglevalue:boolean=false;
+  togglevalue$:Observable<boolean> | null = null;
 
   ngOnInit(): void {
-      this.store.select('counter').subscribe((value)=>{
-        this.togglevalue=value.toggle;
-      })
+      this.togglevalue$ = this.store.select(getToggle);
   }
 
   onCustomValueButtonClicked(){
