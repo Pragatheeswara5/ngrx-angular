5817ab62746cc543c331be34d0ee3f94e2560865
Pragatheeswara5 <ptw.karl@gmail.com>
2026-08-06 09:28:44 +0530

    created course card component

--- src/app/courses/courses.ts
+++ src/app/courses/courses.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CourseCard } from './course-card/course-card';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [CourseCard],
   templateUrl: './courses.html',
   styleUrl: './courses.css',
 })
